57686d943bc0ae4ed6dcc40a1f956e6d90059e1d
Jorge Miranda <jorgaismiranda@gmail.com>
2024-04-09 07:50:29 -0600

    Add: Function calcular_Promedio, button Entrada de datos

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
 function saludar() {
     alert("Bienvenido a JavaScript desde un archivo externo ;)")
 }
+
+function calcular_Promedio() {
+    let call1 = parseInt(prompt("Calificacion de Programacion Web: ", 100))
+    let call2 = parseInt(prompt("Calificacoin de Lenguajes y automatas: "))
+    let call3 = parseInt(prompt("Calificacion de Ingenieria de Softwware: "))
+    
+    let promedio = (call1 + call2 + call3) / 3
+    alert("Tu promedio es: " + promedio)
+}
